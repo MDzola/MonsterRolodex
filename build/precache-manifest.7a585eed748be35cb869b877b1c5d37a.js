8def7cdf5c2d2b8fa79637253ea7bc1e96dffacf
Michael Drew Palazola <mpalazola@hotmail.com>
2020-01-30 16:30:50 -0600

    erased build in gitignore to try deploy

---
+++ build/precache-manifest.7a585eed748be35cb869b877b1c5d37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334bf7b1be8d691bdf0fb8b3b0c3e557",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "034e9714f00b9e8e5667",
+    "url": "/MonsterRolodex/static/css/main.0f49fa6b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/MonsterRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "034e9714f00b9e8e5667",
+    "url": "/MonsterRolodex/static/js/main.713633b9.chunk.js"
+  },
+  {
+    "revision": "8d813800a1105ec84a29",
+    "url": "/MonsterRolodex/static/js/runtime-main.da4ba36f.js"
+  }
+]);
